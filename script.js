1be07d60e014cbd2e6f11e2bf5d2febe21b7453f
MrBurp <andreass.ziegler@gmail.com>
2022-09-15 09:14:03 +0200

    preparing to show movie on the dom

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=8aef7b59166b5d1a199fbb7a4302589f&query="';
 
+const main = document.getElementById("main");
+
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
@@ -13,7 +15,20 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    
+    `;
+  });
 }
 
 form.addEventListener("submit", (e) => {
